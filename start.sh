620b8a9e8e94e9b31f8dc3337352703e789ebc1a
maroprjs <maroprjs@gmail.com>
2023-02-24 19:50:38 +0100

    start delays added

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #sudo node /home/nokia/OLSM/Agv/server.js > /dev/null 2>&1 &
+sleep 5
 node /home/nokia/OLSM/Agv/server.js > /dev/null &
+sleep 10
 python3 /home/nokia/OLSM/Agv/agv.py  > /dev/null &
