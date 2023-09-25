489cb63ad28a5f3fd4a1933e0793bd15f2c74b75
maroprjs <maroprjs@gmail.com>
2023-09-25 10:59:48 +0200

    changed start.sh to add small setup support

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 sleep 5
 node /home/nokia/OLSM/Agv/server.js > /dev/null &
 sleep 10
-python3 /home/nokia/OLSM/Agv/agv.py  > /dev/null &
+#python3 /home/nokia/OLSM/Agv/agv.py  > /dev/null &
+python3 /home/nokia/OLSM/Agv/agv_SmallSetup.py  > /dev/null &
